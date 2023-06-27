cf2e292738c26a79f08d111d96a2c7d9c7ecf013
theophilus doku <theodoku89@gmail.com>
2023-06-27 09:50:40 +0000

    Setup controllers

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,51 @@
+class FoodsController < ApplicationController
+    before_action :set_food, only: %i[show edit update destroy]
+  
+    def index
+      @foods = Food.all
+    end
+  
+    def show
+    end
+  
+    def new
+      @food = Food.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @food = Food.new(food_params)
+  
+      if @food.save
+        redirect_to food_url(@food), notice: 'Food was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @food.update(food_params)
+        redirect_to food_url(@food), notice: 'Food was successfully updated.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @food.destroy
+      redirect_to foods_url, notice: 'Food was successfully destroyed.'
+    end
+  
+    private
+
+    def set_food
+      @food = Food.find(params[:id])
+    end
+  
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price)
+    end
+end
+  
