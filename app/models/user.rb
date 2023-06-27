cf1018e1a45b708d02b5197df01c1daf374a3d2c
theophilus doku <theodoku89@gmail.com>
2023-06-27 11:18:38 +0000

    modify user.rb, recipe.rb food.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  has_many :food
+  has_many :recipe_foods
 end
