313990702742f3a0db19c4b8034b136fa3db2747
theophilus doku <theodoku89@gmail.com>
2023-06-27 10:16:56 +0000

    fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,49 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
-  
-    def index
-      @users = User.all
+  before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
-  
-    def show
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
-  
-    def new
-      @user = User.new
-    end
-  
-    def edit
-    end
-  
-    def create
-      @user = User.new(user_params)
-  
-      if @user.save
-        redirect_to @user, notice: 'User was successfully created.'
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if @user.update(user_params)
-        redirect_to @user, notice: 'User was successfully updated.'
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @user.destroy
-      redirect_to users_url, notice: 'User was successfully destroyed.'
-    end
-  
-    private
-  
-    def set_user
-      @user = User.find(params[:id])
-    end
-  
-    def user_params
-      params.require(:user).permit(:name)
-    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
-  
