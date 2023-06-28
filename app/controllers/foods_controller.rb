afb17367ac911daa5a67c36bc88b17bca999a900
theophilus doku <theodoku89@gmail.com>
2023-06-28 09:14:23 +0000

    fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,51 +1,50 @@
 class FoodsController < ApplicationController
-    before_action :set_food, only: %i[edit update destroy]
-    before_action :authenticate_user!
-  
-    def index
-      @foods = Food.all
-      @food = Food.new
-    end
-  
-    def new
-      @food = Food.new
-    end
-      
-    def edit; end
-  
-    def create
-      @food = Food.new(food_params)
-      @food.user = current_user 
-    
-      if @food.save
-        redirect_to foods_path, notice: "Food successfully created."
-      else
-        render :new
-      end
-    end
-    
-    def update
-      if @food.update(food_params)
-        redirect_to foods_url, notice: 'Food was successfully updated.'
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
+  before_action :set_food, only: %i[edit update destroy]
+  before_action :authenticate_user!
 
-    def destroy
-        @food = Food.find(params[:id])
-        @food.destroy
-        redirect_to foods_path, notice: "Food deleted successfully."
+  def index
+    @foods = Food.all
+    @food = Food.new
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def edit; end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food successfully created.'
+    else
+      render :new
     end
-  
-    private
-  
-    def set_food
-      @food = Food.find(params[:id])
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to foods_url, notice: 'Food was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
-  
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price)
-    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path, notice: 'Food deleted successfully.'
+  end
+
+  private
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
-  
