f5f62e46512b24afeb3d2f119d5c93c0f2d09e57
belalgamal46 <belalgamal00@gmail.com>
2023-06-28 15:11:03 +0300

    Add main_recipes routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :show]
+  resources :main_recipes, only: [:index, :show, :destroy, :update]
 end
