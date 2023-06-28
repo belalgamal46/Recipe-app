626de55888981ab86e5f138bb2f38ec2d2bf5f38
belalgamal46 <belalgamal00@gmail.com>
2023-06-28 15:33:46 +0300

    Add cancancan gem and set it up

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, Food, user_id: user.id
+      can :manage, Recipe, user_id: user.id
+      can :read, :all
+    end
+  end
+end
