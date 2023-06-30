46084766431f5d2a240704988c4b30c7d37d8376
belalgamal46 <belalgamal00@gmail.com>
2023-06-30 20:37:02 +0300

    Fix blank name bug in signup process

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -60,7 +65,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # default url 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
