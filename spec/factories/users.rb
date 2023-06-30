9f0c780c1c0dfe3fcc3d6ee242bfd3a092cc56b5
theophilus doku <theodoku89@gmail.com>
2023-06-30 10:17:39 +0000

    modify spec/factories/users.rb to fix test case not passing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.unique.email }
-    password { 'password123' }
-    # Add any other attributes you need for the User model
+    name { 'John Doe' }
+    email { Faker::Internet.email }
+    password { 'password' }
   end
 end
