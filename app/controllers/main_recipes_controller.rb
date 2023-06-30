1f432e6e69edcd456b8d163bfaedcfdec9d959a6
belalgamal46 <belalgamal00@gmail.com>
2023-06-30 20:26:24 +0300

    Create main_recipe controller for handling recipes view

--- app/controllers/main_recipes_controller.rb
+++ app/controllers/main_recipes_controller.rb
@@ -1,33 +1,45 @@
 class MainRecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show destroy]
+  before_action :set_recipe, only: %i[show destroy update edit]
   before_action :authenticate_user!
 
   def index
     @recipes = Recipe.all
   end
 
-  def destroy
-    if @recipe.destroy
-      redirect_to main_recipes_path, notice: 'Recipe was successfully deleted'
-    else
-      redirect_to main_recipe_path(@recipe), notice: 'Something wrong happened recipe is not deleted'
+  def show
+    @food = Food.all
+    @recipe_food = RecipeFood.includes(:food)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+
+    if @recipe.save
+      redirect_to main_recipe_path(@recipe), notice: 'Recipe was created successfully'
+    else render :new, status: :unprocessable_entity
     end
   end
 
-  def show
-    @foods = Food.all
+  def edit
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      if @recipe.public
-        redirect_to main_recipe_path(@recipe), notice: 'Your recipe is set to public'
-      else
-        redirect_to main_recipe_path(@recipe), notice: 'Your recipe is set to private'
-      end
+      redirect_to main_recipe_path(@recipe), notice: 'Your recipe is set to public successfully'
     else
-      redirect_to main_recipe_path(@recipe), notice: 'Something went wrong'
+      redirect_to main_recipe_path(@recipe), notice: 'Something went wrong your recipe is not updated'
+    end
+  end
+
+  def destroy
+    if @recipe.destroy
+      redirect_to main_recipes_path, notice: 'Recipe was successfully deleted'
+    else redirect_to main_recipe_path(@recipe), notice: 'Something wrong happened recipe is not deleted'
     end
   end
 
@@ -38,6 +50,6 @@ class MainRecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:public)
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
