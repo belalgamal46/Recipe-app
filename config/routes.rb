84c9e95e60736942b777c9d81f29e4f43abcbb71
belalgamal46 <belalgamal00@gmail.com>
2023-06-30 20:34:40 +0300

    Add routes for recipes and general shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
   resources :users, only: [:index, :create, :destroy]
-  resources :main_recipes, only: [:index, :show, :destroy, :update]
+  resources :main_recipes
   resources :foods, except: [:show]
   resources :recipes
+  resources :recipe_foods, only: [:update, :edit, :new, :create, :destroy]
 
-  get '/public_recipes', to: 'public_recipes#index'
+  get '/public_recipes', to: 'public_recipes#index', as: "public_recipes"
+  get '/general_shopping_lists', to: 'general_shopping_lists#index', as: "general_shopping_lists"
 end
